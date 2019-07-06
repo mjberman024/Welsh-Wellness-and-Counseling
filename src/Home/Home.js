28ed2b6837d269c1af1c0cc4b7374072f77ec450
Matthew Berman <mjberman024@gmail.com>
2019-07-06 10:08:07 -0400

    FEAT: Welcome page content added.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import history from '../history';
+import './Home.css';
 
 export default class Home extends Component {
   componentDidMount() {}
@@ -11,9 +12,26 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
-        Welcome to da home
-        {/* <button onClick={this.goHome}>click me to go to about</button> */}
+      <div className="about">
+        <img
+          className="headshot"
+          src="https://welsh-wellness.s3.amazonaws.com/headshot.JPG"
+          alt="Erin headshot"
+        />
+        <div className="about_summary">
+          <h2>Hi I’m Erin</h2>
+          <p className="desc">
+            I help children, adolescents, adults, caregivers, and people wanting
+            to grow their families. I have a wealth of experience, training, and
+            knowledge in many areas, and my goals are to work with my client to
+            reach their highest potential. My therapeutic approach is focused on
+            a collaborative effort to identify individual strengths and needs,
+            develop meaningful goals, and to explore the necessary steps to
+            attain those goals. My approach leads to increased self-esteem,
+            learning, and attaining the supports that are out there — and
+            teaching you to learn how to transition to a positive lens.
+          </p>
+        </div>
       </div>
     );
   }
