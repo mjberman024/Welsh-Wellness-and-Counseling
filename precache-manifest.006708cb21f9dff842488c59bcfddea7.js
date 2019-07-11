d89b898983350beb646892786601d9d6b9705eff
Matthew Berman <mjberman024@gmail.com>
2019-07-11 12:20:03 -0400

    Updates

---
+++ precache-manifest.006708cb21f9dff842488c59bcfddea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f421afdfc1a09aa4e2d3f32c06a2d58b",
+    "url": "/Welsh-Wellness-and-Counseling/index.html"
+  },
+  {
+    "revision": "b83bf6d332973a5b2bb9",
+    "url": "/Welsh-Wellness-and-Counseling/static/css/main.f4671264.chunk.css"
+  },
+  {
+    "revision": "c09ad023840e25c069a0",
+    "url": "/Welsh-Wellness-and-Counseling/static/js/2.cd840e6d.chunk.js"
+  },
+  {
+    "revision": "b83bf6d332973a5b2bb9",
+    "url": "/Welsh-Wellness-and-Counseling/static/js/main.927fb00e.chunk.js"
+  },
+  {
+    "revision": "af1352ca5ceaea961a8e",
+    "url": "/Welsh-Wellness-and-Counseling/static/js/runtime~main.fe44ce6f.js"
+  }
+]);
