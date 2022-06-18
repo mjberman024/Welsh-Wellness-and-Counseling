808a7b13c50652e1ac3042fa7835a0414b6dc114
Matthew Berman <mjberman@Matthews-MBP-2.lan>
2022-06-18 14:50:24 -0400

    Updates

---
+++ precache-manifest.d7018cebf5a57ccf26ca2b0343aace18.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326e6873a4a15faf1515a56114219640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4501bc9c6adb7fb92b22",
+    "url": "/static/css/main.5fbae0e3.chunk.css"
+  },
+  {
+    "revision": "bdbedb1d7f308f2f0ad0",
+    "url": "/static/js/2.e9af6a4d.chunk.js"
+  },
+  {
+    "revision": "4501bc9c6adb7fb92b22",
+    "url": "/static/js/main.660d0a59.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d4b42d9d1acefea4162b2c37afa5ff9",
+    "url": "/static/media/AUTHORIZATION TO RELEASE_EXCHANGE CONFIDENTIAL INFORMATION.6d4b42d9.pdf"
+  },
+  {
+    "revision": "7dafad44ac94cfcb9c9a1dcfc2f34d19",
+    "url": "/static/media/AUTHORIZATION TO RELEASE_EXCHANGE CONFIDENTIAL INFORMATION.7dafad44.jpg"
+  },
+  {
+    "revision": "15b55abea9b71533a5c21d18f0829eb2",
+    "url": "/static/media/consent-for-treatment.15b55abe.pdf"
+  },
+  {
+    "revision": "a30cfe30499f471ec58c416b5a067f77",
+    "url": "/static/media/consent-for-treatment.a30cfe30.jpg"
+  },
+  {
+    "revision": "1ed10fda6c9e645ccff66ea1e5e7215a",
+    "url": "/static/media/hipaaclientrights.1ed10fda.jpg"
+  },
+  {
+    "revision": "fd24a5692e52a60208acb7b948858aad",
+    "url": "/static/media/hipaaclientrights.fd24a569.pdf"
+  },
+  {
+    "revision": "03f4b9b1673105ba6e47665e23e3fd37",
+    "url": "/static/media/hipaaprivacypractices.03f4b9b1.jpg"
+  },
+  {
+    "revision": "0ea7a2a1570d146000afe18a9ae154e9",
+    "url": "/static/media/hipaaprivacypractices.0ea7a2a1.pdf"
+  },
+  {
+    "revision": "8e66138ddf2b373427169b7fe9fd9f73",
+    "url": "/static/media/intakeform.8e66138d.pdf"
+  },
+  {
+    "revision": "9fd6aa6dd40c84e63af9ca146ce65afe",
+    "url": "/static/media/intakeform.9fd6aa6d.jpg"
+  },
+  {
+    "revision": "518ce2d3f727994a0c67fd1e9adb2c5f",
+    "url": "/static/media/signaturesheetsign-offfortherapy.518ce2d3.jpg"
+  },
+  {
+    "revision": "59f8fed22a08c28e93d442be35b678c7",
+    "url": "/static/media/signaturesheetsign-offfortherapy.59f8fed2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39e20e61bedb7cd88a372a40489b700a.js"
+  "/precache-manifest.d7018cebf5a57ccf26ca2b0343aace18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
